a12e19d11f1675a0f13fa88e8255954f97c42494
znxxx <znxxx10@gmail.com>
2023-10-22 17:57:14 +0700

    add API service

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('getuser', views.getData),
+    path('roomid/', views.getRoomId),
     # path('adduser', views.postData),
     # # path('adduserdata', views.addData),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,8 @@
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import math
+import random
+import string
 # from base.models import User, Detail
 # from .serializers import UserSerializer, DetailSerializer
 # from asgiref.sync import sync_to_async
@@ -21,3 +24,11 @@
 #         serializer.save()
 #     return Response(serializer.data)
 
+@api_view(['GET'])
+def getRoomId(request):
+    length = 6
+
+    characters = string.ascii_letters + string.digits
+    room_id = ''.join(random.choice(characters) for _ in range(length))
+
+    return Response({'room_id': room_id})
